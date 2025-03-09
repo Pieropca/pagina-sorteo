0324aed1e934b4590d53c43c43454c340a6024c8
Pieropca <pieropca@gmail.com>
2025-03-09 15:46:43 -0500

    Creada la función para actualización de amigos

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // El principal objetivo de este desafío es fortalecer
 // tus habilidades en lógica de programación. Aquí deberás
 // desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let lista = [];
 
-function intentoDeUsuario() {
+function agregarAmigo() {
     dataAmigo = document.getElementById("amigo");
     if (dataAmigo != ""){
-        listaAmigos.push(dataAmigo);
+        lista.push(dataAmigo);
+        actualizaAmigos(dataAmigo);
         dataAmigo.innerHTML = "";
+        
     }else{
         alert("Por favor, inserte un nombre");
     }
+}
+
+function actualizaAmigos(data) {
+    ul.innerHTML("listaAmigos") = "";
+
+    const li = document.createElement("li"); // Crear un <li>
+    li.textContent = data; // Asignar texto al <li>
+    ul.appendChild(li); // Agregar el <li> al <ul>
 }
