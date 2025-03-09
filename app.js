438de1ac0e963597763df4ce4a743a51f56f86c8
Pieropca <pieropca@gmail.com>
2025-03-09 15:26:01 -0500

    Creada la función de añadido de amigos a una lista

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // El principal objetivo de este desafío es fortalecer
 // tus habilidades en lógica de programación. Aquí deberás
 // desarrollar la lógica para resolver el problema.
-let lista_amigos = [];
+let listaAmigos = [];
 
+function intentoDeUsuario() {
+    dataAmigo = document.getElementById("amigo");
+    if (dataAmigo != ""){
+        listaAmigos.push(dataAmigo);
+        dataAmigo.innerHTML = "";
+    }else{
+        alert("Por favor, inserte un nombre");
+    }
+}
