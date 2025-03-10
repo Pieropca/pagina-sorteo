c63243c0123738f94df68588e79c1df3068f395f
Pieropca <pieropca@gmail.com>
2025-03-09 19:33:03 -0500

    Página completada y mejoras aplicadas

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 // tus habilidades en lógica de programación. Aquí deberás
 // desarrollar la lógica para resolver el problema.
 let amigos = [];
+completado = false;
 
 function agregarAmigo() {
+    if (completado == true) {
+        ulResultado = document.getElementById("resultado");
+        ulResultado.innerHTML = "";
+        amigos = [];
+        completado = false;
+    }
     dataAmigo = document.getElementById("amigo");
     texto = dataAmigo.value;
     if (texto == ""){
@@ -18,6 +25,7 @@ function agregarAmigo() {
             dataAmigo.value = "";
         }
     }
+    return;
 }
 
 function actualizaAmigos() {
@@ -30,4 +38,26 @@ function actualizaAmigos() {
         ul.appendChild(li);
     }
     return;
+}
+
+function sortearAmigo() {
+    if (amigos.length < 1) {
+        alert("La lista está vacía");
+        return;
+    }
+    // Genera un número aleatorio dentro de una lista
+    const amigoAleatorio = amigos[Math.floor(Math.random() * amigos.length)];
+
+    console.log(amigoAleatorio);
+
+    ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+
+    ulResultado = document.getElementById("resultado");
+    ulResultado.innerHTML = "";
+    const liResultado = document.createElement("li");
+    liResultado.textContent = "El amigo secreto sorteado es: " + amigoAleatorio;
+    ulResultado.appendChild(liResultado);
+
+    completado = true;
 }
